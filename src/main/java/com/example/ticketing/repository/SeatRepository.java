35a10b172f52b4473c2cfa6c0015a2604e42187f
haochenglin37 <haochenglin37@gmail.com>
2025-08-11 19:36:23 +0800

    Use updated IgniteAtomicSequence import

--- src/main/java/com/example/ticketing/repository/SeatRepository.java
+++ src/main/java/com/example/ticketing/repository/SeatRepository.java
@@ -3,7 +3,7 @@ package com.example.ticketing.repository;
 import com.example.ticketing.model.Seat;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
-import org.apache.ignite.atomic.IgniteAtomicSequence;
+import org.apache.ignite.IgniteAtomicSequence;
 import org.apache.ignite.cache.query.ScanQuery;
 import javax.cache.Cache;
 import java.util.List;
